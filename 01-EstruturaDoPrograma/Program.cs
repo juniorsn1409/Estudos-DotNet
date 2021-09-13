b26353b404fbd387bc685133a3a35dd0baf53c2b
juniorsn1409 <edsonjuniorananias@gmail.com>
2021-09-12 22:14:30 -0300

    primeiros exemplos

---
+++ 01-EstruturaDoPrograma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EstruturaDoPrograma.Exemplos;
+
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pilha = new Pilha();
+
+            pilha.Empilha(1);
+            pilha.Empilha(2);
+            pilha.Empilha(3);
+
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+        }
+    }
+}
